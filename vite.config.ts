0c3c5d9b2cfd614a4f712bc88550c18587d13308
darknetLinux <roytop1033@gmail.com>
2026-08-11 10:28:17 -0400

    Cambios de vite config y gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://192.168.1.10:8000',
+        target: 'http://192.168.1.103:8000',
         changeOrigin: true,
         ws: true,
       }
